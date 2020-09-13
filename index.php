09c4d408b0d0079640da13539fd63e299b4583bf
typowytester <71172743+typowytester@users.noreply.github.com>
2020-09-13 10:22:26 +0200

    s

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge, chrome=1">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
@@ -16,42 +17,42 @@
         public $pojemnosc; # silnika
         private $wlaczony; # silnik
         private $predkosc;
-    
-    function __construct($_marka, $_model, $_kolor, $_pojemnosc)
-    {   
-        $this->marka = $_marka;
-        $this->model = $_model;
-        $this->kolor = $_kolor;
-        $this->pojemnosc = $_pojemnosc;
-        $this->wlaczony = false;
-        $this->predkosc = 0;
-    }
 
-    function zapal() {
-        $this->wlaczony = true;
-    }
-
-    function zgas() {
-        $this->wlaczony = false;
-    }
-
-    function przyspiesz() {
-        $this->predkosc++;
-    }
-
-    function hamuj() {
-        $this->predkosc--;
-    }
-
-    function stoj() {
-        while($this->predkosc > 0) {
-            $this->hamuj();
+        function __construct($_marka, $_model, $_kolor, $_pojemnosc)
+        {   
+            $this->marka = $_marka;
+            $this->model = $_model;
+            $this->kolor = $_kolor;
+            $this->pojemnosc = $_pojemnosc;
+            $this->wlaczony = false;
+            $this->predkosc = 0;
         }
-    }
 
-    function stan() {
-        $tekst = "Samochód marki $this->marka, model $this->model w kolorze $this->kolor, 
-                    o pojemności silnika $this->pojemnosc ";
+        function zapal() {
+            $this->wlaczony = true;
+        }
+
+        function zgas() {
+            $this->wlaczony = false;
+        }
+
+        function przyspiesz() {
+            $this->predkosc++;
+        }
+
+        function hamuj() {
+            $this->predkosc--;
+        }
+
+        function stoj() {
+            while($this->predkosc > 0) {
+            $this->hamuj();
+            }
+        }
+
+        function stan() {
+            $tekst = "Samochód marki $this->marka, model $this->model w kolorze $this->kolor, 
+                o pojemności silnika $this->pojemnosc ";
 
         if($this->wlaczony)
             $tekst .= "jest włączony i ";
@@ -62,7 +63,7 @@
             $tekst .= "porusza się z prędkością $this->predkosc.";
         else 
             $tekst .= "stoi w miejscu.";
-        
+    
         $tekst .= "<br>";
         return $tekst;
         }
